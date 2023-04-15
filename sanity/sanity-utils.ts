c864a3dfb3eac961e64174b4a54291ffb767d3f2
adarsh maharjan <adarsh986030@gmail.com>
2023-04-15 18:51:57 +0545

    initial commit

---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,19 @@
+import { Project } from "@/types/Project";
+import { createClient, groq } from "next-sanity";
+
+export function getProjects(): Promise<Project[]> {
+  const client = createClient({
+    projectId: "48b35g43",
+    dataset: "production",
+    apiVersion: "2023-03-04",
+  });
+  return client.fetch(groq`*[_type=="project"]{
+    _id,
+    _createdAt,
+    name,
+    "slug":slug.current,
+    "image":image.asset->url,
+    url,
+    content
+  }`);
+}
